0530cef5d90f45b78cea28e2d0ee3a14e799ece7
UizzUW <uizz.uw2@gmail.com>
2016-09-20 20:31:45 +0300

    1.1.0

--- Content/deploy.cmd
+++ Content/deploy.cmd
@@ -1,31 +1,69 @@
-SET DART_SDK_TEMP=D:\local\dart-sdk
+REM ========== SETUP ==========
 
-IF EXIST %DART_SDK_TEMP% (
-    RMDIR %DART_SDK_TEMP% /S /Q
+@echo off
+
+REM check if sdk is on the fast drive - if not, copy it
+IF NOT EXIST %DART_SDK_TEMP% (
+    MD %DART_SDK_TEMP% 2>nul
+    XCOPY %HOME%\dart-sdk\*.* %DART_SDK_TEMP%\ /s /e /f
 )
 
-MD %DART_SDK_TEMP% 2>nul
-XCOPY %HOME%\dart-sdk\*.* %DART_SDK_TEMP%\ /s /e /f
+REM ========== SERVER ==========
 
-IF NOT DEFINED DART_SERVER_PROJECT_PATH (
-    ECHO DART_SERVER_PROJECT_PATH not defined, skipping server deployment.
-) ELSE (
+REM check if a custom server source path is set, or if the default one exists
+SET SERVER_SOURCE="%DEPLOYMENT_SOURCE%\%DART_SERVER_SOURCE_PATH%"
+
+IF NOT EXIST %SERVER_SOURCE% (
+    ECHO Server source not found - looked in %SERVER_SOURCE%
+    ECHO You can define the DART_SERVER_SOURCE_PATH variable and point it
+    ECHO to a custom location relative to your repository.
+    ECHO Skipping server deployment.
+
+    SET SERVER_SOURCE=
+)
+
+REM if we have found a server to deploy
+IF DEFINED SERVER_SOURCE (
     ECHO Deploying server project...
-    CD %DEPLOYMENT_SOURCE%\%DART_SERVER_PROJECT_PATH%
+    CD %SERVER_SOURCE%
+
+    REM restore packages
     CALL %DART_SDK_TEMP%\bin\pub get --no-packages-dir
-    XCOPY .\*.* %WEBROOT_PATH%\server\ /s /e /f
-    ECHO Server deployment completed successfully.
 )
 
-IF NOT DEFINED DART_CLIENT_PROJECT_PATH (
-    ECHO DART_CLIENT_PROJECT_PATH not defined, skipping client deployment.
-) ELSE (
+REM ========== CLIENT ==========
+
+REM check if a custom client source path is set, or if the default one exists
+SET CLIENT_SOURCE="%DEPLOYMENT_SOURCE%\%DART_CLIENT_SOURCE_PATH%"
+
+IF NOT EXIST %CLIENT_SOURCE% (
+    ECHO Client source not found - looked in %CLIENT_SOURCE%
+    ECHO You can define the DART_CLIENT_SOURCE_PATH variable and point it
+    ECHO to a custom location relative to your repository.
+    ECHO skipping client deployment.
+
+    SET CLIENT_SOURCE=
+)
+
+REM if we have found a client to deploy
+IF DEFINED CLIENT_SOURCE (
     ECHO Deploying client project...
-    cd %DEPLOYMENT_SOURCE%\%DART_CLIENT_PROJECT_PATH%
+    CD %CLIENT_SOURCE%
+
+    REM restore packages
     CALL %DART_SDK_TEMP%\bin\pub get --no-packages-dir
+
+    REM compile client into JS
     CALL %DART_SDK_TEMP%\bin\pub build
-    XCOPY .\build\web\*.* %WEBROOT_PATH%\client\ /s /e /f
-    ECHO Client deployment completed successfully!
 )
 
-ECHO Dart.Azure deployment task finished successfully.
+REM delete old version and copy the new one
+IF EXIST %DART_APP% (
+    RMDIR %DART_APP% /S /Q
+)
+MD %DART_APP% 2>nul
+
+CD %DEPLOYMENT_SOURCE%
+XCOPY .\*.* %DART_APP%\ /s /e /f
+
+echo Dart deployment finished.
--- Content/install.cmd
+++ Content/install.cmd
@@ -1,22 +1,32 @@
 @ECHO OFF
 
-IF EXIST %HOME%\dart-sdk (
-	RMDIR %HOME%\dart-sdk /S /Q
+SET DART_SDK="%HOME%\dart-sdk"
+SET DART_SDK_TEMP="D:\local\dart-sdk"
+
+REM cleanup old version
+IF EXIST %DART_SDK% (
+	RMDIR %DART_SDK% /S /Q
 )
 
+IF EXIST %DART_SDK_TEMP% (
+	RMDIR %DART_SDK_TEMP% /S /Q
+)
+
+REM get latest Dart version
 ECHO Downloading Dart SDK...
 CALL curl -k -o .\dart.zip https://storage.googleapis.com/dart-archive/channels/stable/release/1.19.0/sdk/dartsdk-windows-ia32-release.zip
 
+REM unzip it
 ECHO Unzipping Dart SDK...
 CALL unzip -o .\dart.zip -d %HOME%\
 
+REM inject the deployment task and web.config
 ECHO Configuring extension...
 SET DEPLOY_ACTION_LOCATION=%HOME%\site\deployments\tools
 MD %DEPLOY_ACTION_LOCATION% 2>nul
-MOVE .\dart_deploy.cmd %DEPLOY_ACTION_LOCATION%\deploy.cmd
-MOVE .\web.config %WEBROOT_PATH%\web.config
+MOVE .\deploy.cmd %DEPLOY_ACTION_LOCATION%\deploy.cmd
+MOVE .\applicationHost.xdt %HOME%\site\applicationHost.xdt
+MOVE .\web.config %HOME%\site\wwwroot\web.config
 
-ECHO Cleaning up...
+REM clean up
 DEL .\dart.zip
-
-ECHO Done.
